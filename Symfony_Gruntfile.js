392a67b6469219f03445dac168e9a28d2170d46c
Vadim Hermann <vhermann@wheregroup.com>
2014-04-07 11:05:46 +0200

    init grunt kickstarter

---
+++ Symfony_Gruntfile.js
@@ -0,0 +1,162 @@
+//
+// This is a Gruntfile.js template for symfony2 projects included
+// compass as a frontend *framework*.
+//
+'use strict';
+
+// TODO: phpmd rulesets
+//       shell trans bundlename as a cons?
+//       ftp deploy
+//       browser sync
+//       seperate sass clean cache
+
+module.exports = function (g) {
+    g.loadNpmTasks('grunt-contrib-clean');
+    g.loadNpmTasks('grunt-contrib-copy');
+    g.loadNpmTasks('grunt-contrib-cssmin');
+    g.loadNpmTasks('grunt-contrib-jshint');
+    g.loadNpmTasks('grunt-contrib-compass');
+    g.loadNpmTasks('grunt-contrib-uglify');
+    g.loadNpmTasks('grunt-contrib-watch');
+    g.loadNpmTasks('grunt-contrib-imagemin');
+    g.loadNpmTasks('grunt-phpmd');
+    g.loadNpmTasks('grunt-open');
+    g.loadNpmTasks('grunt-files-check');
+    g.loadNpmTasks('grunt-autoprefixer');
+    g.loadNpmTasks('grunt-phplint');
+    g.loadNpmTasks('grunt-shell');
+
+
+
+    g.option( 'force', true );
+    g.initConfig({
+        // ------------------------------------------------------------- Globals
+        pkg:                 g.file.readJSON('package.json'),
+        project_root_folder: "..",
+        project_browser_url: "http://localhost/hal/app_dev.php",
+        project_source:      "<%= project_root_folder %>/source",
+        project_dist:        "<%= project_root_folder %>/dist",
+
+        // ---------------------------------------------------- Symfony2 globals
+        symfony_console: "<%= project_source %>/app/console",
+
+
+
+        // ---------------------------------------------------------- Task Files
+        // ---------------------------------------------------------------------
+        // ------------------------------------------------------------### Clean
+        clean_cons: {
+            "source": "<%= project_source %>",
+            "dist":   "<%= project_dist %>"
+        },
+        clean: g.file.readJSON('tasks/symfony/clean.json'),
+        // -----------------------------------------------------### Open Browser
+        open: g.file.readJSON('tasks/open.json'),
+        // -------------------------------------------------------------### Copy
+        copy_cons: {
+            "root": "<%= project_source %>/",
+            "dest": "<%= project_dist %>/"
+        },
+        copy: g.file.readJSON('tasks/copy.json'),
+        // ----------------------------------------------------------### CSS Min
+        cssmin_cons: {
+            "cwd":  "<%= project_dist %>/web",
+            "dest": "<%= project_dist %>/web",
+        },
+        cssmin: g.file.readJSON('tasks/cssmin.json'),
+        // --------------------------------------------------------### Image Min
+        imagemin_cons: {
+            "cwd":  "<%= project_source %>/src",
+            "dest": "<%= project_source %>/src",
+        },
+        imagemin: g.file.readJSON('tasks/imagemin.json'),
+        // -----------------------------------------------------------### JSHint
+        //
+        // WATCHOUT: .jshintrc is needed in the Gruntfile.js folder
+        //
+        jshint_cons: {
+            "targets": ["<%= project_source %>/src/**/*.js"],
+            "ignores": ["<%= project_source %>/src/**/vendors/*.js"],
+        },
+        jshint: g.file.readJSON('tasks/jshint.json'),
+        // -----------------------------------------------------### Autoprefixer
+        autoprefixer_cons: {
+            "browsers": ["last 2 version", "ie 9"],
+            "src":      "<%= project_source %>/src/WhereGroup/CoreBundle/Resources/public/styles/*.css",
+            "dest":     "<%= project_source %>/src/WhereGroup/CoreBundle/Resources/public/styles/",
+        },
+        autoprefixer: g.file.readJSON('tasks/autoprefixer.json'),
+        // ----------------------------------------------------------### Compass
+        compass_cons: {
+            "import_path": "<%= project_source %>/src/WhereGroup/CoreBundle/Resources/public/sass",
+            "location":    "<%= project_source %>/src/WhereGroup/CoreBundle/Resources/public"
+        },
+        compass: g.file.readJSON('tasks/compass.json'),
+        // ------------------------------------------------------### Files Check
+        filescheck_cons: {
+            "script_pattern": "css\\(|show\\(|hide\\(",
+            "script_src":     ["<%= project_source %>/src/**/scripts/*.js"],
+            "html_pattern":   "style=|<style",
+            "html_src":       ["<%= project_source %>/src/**/*.html.twig"],
+        },
+        files_check: g.file.readJSON('tasks/files-check.json'),
+        // ----------------------------------------------------------### Phplint
+        phplint_cons: {
+            "swap":  "<%= project_source %>/app/cache",
+            "files": ["<%= project_source %>/app/**/*.php",
+                      "<%= project_source %>/src/**/*.php"],
+        },
+        phplint: g.file.readJSON('tasks/phplint.json'),
+        // ------------------------------------------------------------### Phpmd
+        phpmd_cons: {
+            "dir":     "<%= project_source %>/src",
+            "exclude": "Entity,Form",
+        },
+        phpmd: g.file.readJSON('tasks/phpmd.json'),
+        // --------------------------------------------------------### Uglify Js
+        uglify_cons: {
+            "src":  "<%= project_dist %>/web/",
+            "dest": "<%= project_dist %>/web/",
+        },
+        uglify: g.file.readJSON('tasks/uglify.json'),
+        // ------------------------------------------------------------### Watch
+        watch_cons: {
+            "reload_port": 1337,
+            "src":         "<%= project_source %>/src/",
+        },
+        watch: g.file.readJSON('tasks/watch.json'),
+        // ------------------------------------------------------------### Shell
+        //
+        // WATCHOUT: Customize the shell.json file
+        //
+        shell_cons: {"path": "<%= symfony_console %>"},
+        shell: g.file.readJSON('tasks/symfony/shell.json'),
+    });
+
+
+
+    // -------------------------------------------------------- Registered Tasks
+    // -------------------------------------------------------------------------
+    g.registerTask('dev', ['clean:cache',
+                           'open',
+                           'compass',
+                           'autoprefixer',
+                           'shell:symlinks',
+                           'watch']);
+    g.registerTask('build', ['shell:symlinks',
+                             'shell:assetic',
+                             'clean:dist',
+                             'copy:build',
+                             'clean:build',
+                             'imagemin',
+                             'cssmin',
+                             'uglify']);
+    g.registerTask('symlinks' ['shell:symlinks']);
+    g.registerTask('assets',  ['shell:assetic']);
+    g.registerTask('cache',   ['clean:cache']);
+    g.registerTask('trans',   ['shell:trans']);
+    g.registerTask('md',   ['phpmd']);
+
+    // g.registerTask('deploy', ['ftp-deploy']);
+    // g.registerTask('front-test', ['browserSync:test']);
+};
